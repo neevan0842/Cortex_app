44fa64217caa54929b86f1145190c24fab9de86c
Naveen Manoj <manojnaveen085@gmail.com>
2025-07-22 14:29:35 +0530

    restart

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,5 @@
-import { ThemeProvider } from "@/contexts/ThemeProvider";
 import { Stack } from "expo-router";
-import "./global.css";
 
 export default function RootLayout() {
-  return (
-    <ThemeProvider>
-      <Stack />
-    </ThemeProvider>
-  );
+  return <Stack />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,15 @@
-import { useTheme } from "@/contexts/ThemeProvider";
-import { Pressable, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function Index() {
-  const { toggleTheme } = useTheme(); // Ensure the theme context is used
   return (
-    <View className="flex-1 justify-center items-center bg-background">
-      <Text className="text-primary-foreground text-xl">
-        Welcome to the Home Page!
-      </Text>
-      <Pressable
-        onPress={toggleTheme}
-        className="mt-4 px-4 py-2 bg-primary rounded"
-      >
-        <Text className="text-primary-foreground">Toggle Theme</Text>
-      </Pressable>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
 }
